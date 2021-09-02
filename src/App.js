dcca96e8c9eec53ed7120b0522cb096d27e4879a
Moises Gaioli Estevão <moi.rp@hotmail.com>
2021-09-01 23:26:47 -0300

    Finalizado

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import CenteredCard from './components/CenteredCard';
+
+function App() {
+
+  return (
+
+    <div className="App">
+       
+       <CenteredCard children="Moisés" />
+
+    </div>
+  );
+}
+
+export default App;
